ec21b2b7ca6e9cbc4694b5cbc276dc1e95777b16
michael <michaelutoh21@gmail.com>
2024-01-11 23:53:40 +0100

    Add pagination to fetch users endpoint

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -9,10 +9,22 @@ const { createHashedPassword, validatePassword } = require('../middleware/hashPa
 const UserController = {
   getAllUsers: [authenticateUser, async (req, res) => {
     try {
-      const users = await UserModel.find({ archive: false }).select(
-        '_id email first_name last_name address1 state country created_at'
-      );
-      res.status(200).json(users);
+      const page = parseInt(req.query.page) || 1;
+      const limit = parseInt(req.query.limit) || 10;
+      const skip = (page - 1) * limit;
+
+      const users = await UserModel.find({ archive: false })
+        .select('_id email first_name last_name address1 state country created_at')
+        .skip(skip)
+        .limit(limit);
+
+      const count = await UserModel.countDocuments({ archive: false });
+      res.status(200).json({
+        users,
+        count,
+        totalPages: Math.ceil(count / limit),
+        currentPage: page
+      });
     } catch (error) {
       console.error('Error fetching users:', error);
       res.status(500).json({ error: 'Internal Server Error' });
